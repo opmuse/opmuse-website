c8084424621de9b2f66d4627c64a1e7a4f5d774e
Mattias Fliesberg <mattias.fliesberg@gmail.com>
2012-12-15 16:28:11 +0100

    simple twitter bootstrap site that renders our github README

---
+++ render.py
@@ -0,0 +1,28 @@
+from urllib.request import urlopen
+import os
+import json
+import subprocess
+
+root = os.path.dirname(__file__)
+
+markdown = urlopen("https://raw.github.com/opmuse/opmuse/master/README.md").read()
+
+html = urlopen("https://api.github.com/markdown", json.dumps({
+    "text": markdown.decode('utf8'),
+    "mode": "markdown"
+}).encode('utf8')).read()
+
+template = open('template.html', 'r').read()
+
+with open(os.path.join(root, "public", "index.html"), "w") as index:
+    index.write(template.replace('MARKDOWN', html.decode('utf8')))
+
+subp = subprocess.Popen([
+    os.path.join(root, "vendor", "less.js", "bin", "lessc"),
+    os.path.join(root, "main.less")
+], stdout = subprocess.PIPE)
+
+with open(os.path.join(root, "public", "main.css"), "wb") as css:
+    css.write(subp.stdout.read())
+
+subp.wait()
